88e9c0eff70aa269f7f01edda53d031fbaf2c656
dharzan <dguruparan@beckettcorp.com>
2024-02-08 13:31:18 -0500

    dark mode

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { TextureLoader } from 'three';
 import About from './About';
 import './App.css';
 import diya from './diya.png';
+import diya2 from './diya2.jpeg';
 import useScrollPosition from './useScrollPosition';
 
 
-function Cube({ imagePaths, spin }) {
+function Cube({ spin }) {
   const meshRef = useRef();
  
   const texture4 = useLoader(TextureLoader, diya);
+  const texture3 = useLoader(TextureLoader, diya2);
 
 
   useFrame(() => {
@@ -26,12 +28,12 @@ function Cube({ imagePaths, spin }) {
 
       <boxGeometry args={[2, 2, 2]} />
 
-      <meshBasicMaterial attach="material-0" map={texture4} />
-      <meshBasicMaterial attach="material-1"map={texture4}/>
+      <meshBasicMaterial attach="material-0" map={texture3} />
+      <meshBasicMaterial attach="material-1"map={texture3}/>
       <meshBasicMaterial attach="material-2" map={texture4} />
       <meshBasicMaterial attach="material-3" map={texture4}/>
-      <meshBasicMaterial attach="material-4" map={texture4} />
-      <meshBasicMaterial attach="material-5" map={texture4} />
+      <meshBasicMaterial attach="material-4" map={texture3} />
+      <meshBasicMaterial attach="material-5" map={texture3} />
       
 
 
@@ -61,14 +63,13 @@ const Scene = () => {
 
   const spin = useScrollPosition();
   const[isDarkMode, setIsDarkMode] = React.useState(false);
-
-
-
   const toggleDarkMode = () => { setIsDarkMode(!isDarkMode)};
 
   return (
     <div style={{ height: '200vh', background: isDarkMode ? '#333': 'transparent' }}>
-    <button onClick={toggleDarkMode}/>
+    <button onClick={toggleDarkMode}>
+      DarkMode
+    </button>
     <About/>
       <Canvas style={{ position: 'fixed', top: 0, left: 0, zIndex: -1}}>
         <ambientLight intensity={0.5} />
