7c9eb797fc9b6be8131e9ebfe06afc38c5120483
dharzan <dguruparan@beckettcorp.com>
2024-02-09 08:36:33 -0500

    updated

--- src/About.js
+++ src/About.js
@@ -1,45 +1,32 @@
-import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import React from 'react';
 import useScrollPosition from './useScrollPosition';
 
-
-const SpinningCube = ({ scroll }) => {
-    const meshRef = useRef();
-    useFrame(() => {
-
-        const rotationSpeed = scroll * 0.00001;
-        meshRef.current.rotation.x += rotationSpeed;
-        meshRef.current.rotation.y += rotationSpeed
-
-    })
-
-
-
-    return (
-        <mesh ref={meshRef}>
-            <boxGeometry args={[2, 2, 2]} />
-            <meshStandardMaterial color={'hotpink'} />
-        </mesh>
-
-    )
-}
 
 const About = ({ isDarkMode }) => {
     const spin = useScrollPosition()
     return (
         <div style={{ height: '600vh' }}>
-            <h1 style={{color: isDarkMode ? 'green' : 'black'}}>About Me</h1>
+            <h1 style={{ color: isDarkMode ? 'green' : 'black' }}>About Me</h1>
 
-            <p style={{color: isDarkMode ? 'white' : 'black'}}>Hi, I'm Diya, Bioengineering Student </p>
+            <p style={{ color: isDarkMode ? 'white' : 'black' }}>Hi, I'm Diya, Bioengineering Student </p>
 
             <div id="3D-model-Avatar-placeholder"> </div>
         </div>
 
     )
 }
-const Facts = () => {
 
-    
+// TimeLine p
+export function Timeline({ isDarkMode }) {
+
+     
+
+
+
+
+
+
+
 }
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import React, { useRef } from 'react';
 import { TextureLoader } from 'three';
-import About from './About';
+import About, { Timeline } from './About';
 import './App.css';
 import diya from './diya.png';
 import diya2 from './diya2.jpeg';
 import useScrollPosition from './useScrollPosition';
 
 
-function Cube({ spin }) {
+export function Cube({ spin }) {
   const meshRef = useRef();
  
   const texture4 = useLoader(TextureLoader, diya);
   const texture3 = useLoader(TextureLoader, diya2);
 
-
- 
-
-
   useFrame(() => {
-    const rotationSpeed = spin * 0.005;
+    const rotationSpeed = 0.005;
     
-    meshRef.current.rotation.y = rotationSpeed;
+    meshRef.current.rotation.y += rotationSpeed;
   });
 
 
@@ -29,7 +25,7 @@ function Cube({ spin }) {
     <mesh
       ref={meshRef}>
 
-      <boxGeometry args={[2, 2, 2]} />
+      <boxGeometry args={[3, 3, 3]} />
 
       <meshBasicMaterial attach="material-0" map={texture3} color={"#999999"} />
       <meshBasicMaterial attach="material-1" map={texture4} color={"#999999"} />
@@ -45,26 +41,8 @@ function Cube({ spin }) {
 
 };
 
-export function Scene1 (){
 
-  const spin = useScrollPosition();
-  return (
-    <div style={{ height: '100vh' }}>
-    
-      <Canvas style={{ position: 'fixed', top: 0, left: 0, zIndex: -1 }}>
-        
-        
-        <ambientLight intensity={.5} />
-        <pointLight position={[2, 2, 10]} />
-        <Cube  spin={spin} />
-      </Canvas>
-
-    </div>
-  )
-
-}
-
-const Scene = () => {
+export function Scene(){
 
   const spin = useScrollPosition();
   const[isDarkMode, setIsDarkMode] = React.useState(false);
@@ -80,7 +58,8 @@ const Scene = () => {
       <perspectiveCamera position={[0,0,5]}/>
         <ambientLight intensity={0.5} />
         <pointLight position={[3, 4, 2]} />
-        <Cube spin={spin} />
+        <Cube   />
+        <Timeline isDarkMode={isDarkMode}/>
       </Canvas>
 
     </div>
@@ -88,7 +67,7 @@ const Scene = () => {
   )
 }
 
-function App() {
+export default function App() {
 
   return (
 
@@ -98,4 +77,4 @@ function App() {
   );
 }
 
-export default App;
+
